8b86f843325c5f826eee5ef5a20c6e935c8e4075
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-01-22 09:57:12 +0000

    add async await to fetch data from the db.json-server

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,9 +2,16 @@ import { FaTimes } from 'react-icons/fa';
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div class={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={ () => onToggle(task.id)}>
+    <div
+      className={`task ${task.reminder ? 'reminder' : ''}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
       <h4>
-        {task.text} <FaTimes onClick={() => onDelete(task.id)} style={{color: 'red', cursor: 'pointer'}}/>
+        {task.text}{' '}
+        <FaTimes
+          onClick={() => onDelete(task.id)}
+          style={{ color: 'red', cursor: 'pointer' }}
+        />
       </h4>
       <p>{task.day}</p>
     </div>
