4f0fb24488366575b8f1b4566f2eef34425dbb0d
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-01-22 08:16:53 +0000

    added a dynamic call to the button

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ name, onAdd, showAdd }) => {
     return (
         <header className='header'>
             <h1> Helloooo {name}</h1>
-            <Button color="rgb(38, 224, 69)" text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+            <Button color={showAdd ? 'red' : "rgb(38, 224, 69)"} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
