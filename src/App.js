854791e99568704689275d897fd29223891b8e0c
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-01-22 09:49:09 +0000

    creating a db.json-server

--- src/App.js
+++ src/App.js
@@ -6,26 +6,7 @@ import AddTask from './components/AddTask';
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Leg Training at Genesis',
-      day: 'Monday 4th 6pm',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Buy Flowers',
-      day: 'Wednesday Eastern Time',
-      reminder: false
-    },
-    {
-      id: 3,
-      text: 'Cook Dinner',
-      day: 'Saturday, 7pm',
-      reminder: true
-    }
-  ])
+  const [tasks, setTasks] = useState([])
 
   const deleteTask = id => {
     setTasks(tasks.filter(task => task.id !== id))
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
-import Button from './Button'
+import Button from './Button';
 
 const Header = ({ name, onAdd, showAdd }) => {
-
-    return (
-        <header className='header'>
-            <h1> Helloooo {name}</h1>
-            <Button color={showAdd ? 'red' : "rgb(38, 224, 69)"} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <h1> Task {name}</h1>
+      <Button
+        color={showAdd ? 'red' : 'rgb(38, 224, 69)'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    name: 'Ronnie'
-}
+  name: 'Tracker'
+};
 
-export default Header
+export default Header;
